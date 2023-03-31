9ed0078848a147bb6630da077226150f4e800f18
tinkukalluri <tinkukalluri19859@gmail.com>
2023-03-31 23:55:23 +0530

    Update results.py

--- results.py
+++ results.py
@@ -1,6 +1,12 @@
 import pandas as pd
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 2000)
+pd.set_option('display.float_format', '{:20,.2f}'.format)
+pd.set_option('display.max_colwidth', None)
 
-df=pd.read_excel('C:/Users/sintin/OneDrive/Desktop/2_1_results_1.xlsx')
+# df=pd.read_excel('C:/Users/sintin/OneDrive/Desktop/2_1_results_1.xlsx')
+df=pd.read_excel(r"C:\Users\sintin\Downloads\results-4-1.xlsx")
 print(df)
 
 dic={"111":[1 ,2 ,3]}
@@ -9,7 +15,7 @@ passed=0
 failed=0
 failed_log={"tinku":1}
 for i in range(0 , len(df)):
-    if(df["EXTERNALMARKS"][i]<26 and df["SUBJECT_NAME"][i]!="GENDER SENSITIZATION LAB"):
+    if(df["EXTERNALMARKS"][i]<26 and df["SUBJECT_NAME"][i]!="SEMINAR"): #and df["SUBJECT_NAME"][i]!="GENDER SENSITIZATION LAB"
         failed_log[df["HTNO"][i]]=1
         if(dic.get(df["HTNO"][i])!=None):
             dic.pop(df["HTNO"][i])
@@ -21,6 +27,8 @@ for i in range(0 , len(df)):
 
 max=[-1 , "tinku kalluri"]
 max_index=0
+print(dic)
+ranks = []
 for row in dic:
     if(row[1]=="9"):
         passed+=1
@@ -31,6 +39,7 @@ for row in dic:
             total+=i
         print("========================================" , total/100)
         p=total/100
+        ranks.append([row , p])
         if(max[0]<total/100):
             max.clear()
             max.append(p)
@@ -39,6 +48,21 @@ for row in dic:
             max.append(p)
             max.append(row)
     # print(df['HTNO'][i] , df["SUBJECT_NAME"])
+
+# If it returns a positive number: x > y
+# If it returns 0: x == y
+# If it returns a negative number: x < y
+
+def compare_ranks(stu1 , stu2):
+    stu1_tot = stu1[1]
+    stu2_tot = stu2[1]
+    return stu1_tot - stu2_tot
+
+ranks.sort(key=lambda x: x[1] , reverse=True)
+
+print("----------ranks----------------------")
+for rank in ranks:
+    print(rank)
 
 print("============congratulations===========")
 print(max)
